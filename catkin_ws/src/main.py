bec850c89f39266542e802234f3f2968b587ab0e
antoine.paulus <antoine.paulus@univ-tlse3.fr>
2022-10-28 18:44:42 +0200

    Nettoyage + documentation code python

--- catkin_ws/src/main.py
+++ catkin_ws/src/main.py
@@ -1,4 +1,3 @@
-# Python 2/3 compatibility imports
 from __future__ import print_function
 from six.moves import input
 
@@ -16,16 +15,6 @@ from moveit_commander.conversions import pose_to_list
 
 WAYPOINT_LIST = [(0,0.5,0),
                  (0,-0.5,0)] # [m], list of wp, offset from the last wp
-SCALE = 1
-
-moveit_commander.roscpp_initialize(sys.argv)
-rospy.init_node("HC_10_controler_python", anonymous=True)
-
-display_trajectory_publisher = rospy.Publisher(
-    "/move_group/display_planned_path",
-    moveit_msgs.msg.DisplayTrajectory,
-    queue_size=20,
-)
 
 class HC10Robot():
     def __init__(self):
@@ -33,16 +22,17 @@ class HC10Robot():
         self.scene = moveit_commander.PlanningSceneInterface()
         self.group_name = "hc10_arm"
         self.move_group = moveit_commander.MoveGroupCommander(self.group_name)
+        self.publisher = rospy.Publisher("/move_group/display_planned_path",
+                                         moveit_msgs.msg.DisplayTrajectory,
+                                         queue_size=20,
+                                        )
         rospy.sleep(2)
 
-    def load_cartesian_path(self,scale : float,waypoint_list : list):
-        '''self.__refresh_scene_object()
-        for object in self.scene.get_known_object_names():
-            self.__wait_state_update(object)
-        '''
-        waypoints = []
+    def load_cartesian_path(self, waypoint_list : list):
+        """Load and define a path between a list of points given as a parameter"""
+        waypoints = [] # Waypoints in the correct format
         wpose = self.move_group.get_current_pose().pose
-        waypoints.append(copy.deepcopy(wpose))
+        waypoints.append(copy.deepcopy(wpose)) #For each waypoint, we create a new waypoint from the latest.
         for wp in waypoint_list : 
             wpose.position.x += wp[0]
             wpose.position.y += wp[1]
@@ -52,20 +42,20 @@ class HC10Robot():
         waypoints, 0.01, 0.0, avoid_collisions = True)
         return plan, fraction
     
-    def display_trajectory(self, plan, publisher):
+    def display_trajectory(self, plan):
+        """Send a message to RVIZ to display the trajectory of the robot"""
+        # We build the trajectory message
         display_trajectory = moveit_msgs.msg.DisplayTrajectory()
         display_trajectory.trajectory_start = self.robot.get_current_state()
         display_trajectory.trajectory.append(plan)
-        # Publish
-        publisher.publish(display_trajectory)
 
-        # If we exited the while loop without returning then we timed out
-        return False
+        #Then we publish it
+        self.publisher.publish(display_trajectory)
 
-    #def __refresh_scene_object(self):
-       # self.scene.get_objects([])
+        rospy.sleep(0.1)
 
     def add_box_manually(self, box_name_user):
+        """Add a simple box to the scene."""
         box_pose = geometry_msgs.msg.PoseStamped()
         box_pose.header.frame_id = "world"
         box_pose.pose.orientation.w = 1.0
@@ -76,6 +66,7 @@ class HC10Robot():
         self.scene.add_box(box_name, box_pose, size=(0.075, 0.075, 0.075))
 
     def check_object_added(self, name :str, box_is_attached : bool = False, box_is_known : bool = True):
+        """Basic function from the tutorial to run a loop and check if a box was added."""
         start = rospy.get_time()
         seconds = rospy.get_time()
         timeout = 10
@@ -101,13 +92,15 @@ class HC10Robot():
         return False
         
     def execute_plan(self, plan):
+        """Execute plan - non implémentée"""
         while not self.move_group.execute(plan):
             continue
 
+moveit_commander.roscpp_initialize(sys.argv)
+rospy.init_node("HC_10_controler_python", anonymous=True)
 
 robot = HC10Robot()
-plan, fraction = robot.load_cartesian_path(SCALE,WAYPOINT_LIST)
-# robot.add_box_manually("Potatoes")
-#print(robot.check_object_added("Potatoes"))
-robot.display_trajectory(plan,display_trajectory_publisher)
+plan, fraction = robot.load_cartesian_path(WAYPOINT_LIST)
+robot.add_box_manually("box")
+robot.display_trajectory(plan)
 robot.execute_plan(plan)
